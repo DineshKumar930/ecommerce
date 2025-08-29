36bf74eea2f232d20112718172c45da5c31a9ceb
Dinesh Kumar <159294017+DineshKumar930@users.noreply.github.com>
2025-08-29 10:02:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/crazycart/",   // 👈 IMPORTANT for GitHub Pages
+  base: "/Ecommerce-01/",   // 👈 IMPORTANT for GitHub Pages
 });
