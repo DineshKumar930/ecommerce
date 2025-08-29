2b227a6825afa68ad3eeb7e091777133ca9db63b
Dinesh Kumar <159294017+DineshKumar930@users.noreply.github.com>
2025-08-29 09:46:56 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import App from "./App.jsx";         // make sure App.jsx exists in src/
+import { store } from "./store/store.js"; // make sure store.js exists in src/store/
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <Provider store={store}>
     <App />
